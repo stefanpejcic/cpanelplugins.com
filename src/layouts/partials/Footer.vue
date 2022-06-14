3a52d50255f1abfe507b067a4179a76815d9157f
stefanpejcic <pejcha1994@gmail.com>
2022-06-14 15:34:47 +0200

    1st plugin

--- src/layouts/partials/Footer.vue
+++ src/layouts/partials/Footer.vue
@@ -10,7 +10,7 @@
 
     <g-link to="/logo/" style="margin-left: .5rem">Logo & assets</g-link>
 
-</br>
+</br></br>
     cPanel, WebHost Manager and WHM are registered trademarks of <g-link to="https://cpanel.net/">cPanel, Inc.</g-link></br>
 This website is not affiliated with cPanel Inc.
   </footer>
